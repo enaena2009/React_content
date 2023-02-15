550422849093ba0bbb332a30c1a7318bc3d24a13
enaena2009 <119826821+enaena2009@users.noreply.github.com>
2023-02-15 14:03:47 +0900

    nabe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Message from "./components/message";
+import nabeatu from "./images/nabeatsu.webp";
 
 const App = () => {
   // const contentstyle = {
@@ -7,12 +8,55 @@ const App = () => {
   //   Fontsize:"18px"
   //   // React上の記法として、font-sizeをFontsizeにしている。
   // };
+  const onClickCountUp = () => {
+    setNum(Num + 1);
+  };
+
+  const onClickCountDown = () => {
+    setNum(Num - 1);
+  };
+
+  const onClickCountReset = () => {
+    setNum(Num - Num);
+    FaseShowFlag && setFaseShowFlag(false);
+  };
+
+  const [Num, setNum] = useState(0);
+
+  const onClickSwichFaseShowFlag = () => {
+    setFaseShowFlag(!FaseShowFlag);
+  };
+  const [FaseShowFlag, setFaseShowFlag] = useState(false);
+
+  useEffect(() => {
+    if (Num > 0) {
+      if (Num % 3 === 0) {
+        FaseShowFlag || setFaseShowFlag(true);
+      } else {
+        FaseShowFlag && setFaseShowFlag(false);
+      }
+    }
+  }, [Num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       {/* <p　style={contentstyle}>お元気ですか</p> */}
       <Message color="pink">元気です</Message>
       <Message color="blue">元気です</Message>
+
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickCountDown}>カウントダウン</button>
+      <button onClick={onClickCountReset}>リセット</button>
+      <br />
+      <button onClick={onClickSwichFaseShowFlag}>on/off</button>
+      <p>{Num}</p>
+      {FaseShowFlag && (
+        <p>
+          <img src={nabeatu} alt="" />
+        </p>
+      )}
+      {FaseShowFlag || <p></p>}
     </>
   );
 };
